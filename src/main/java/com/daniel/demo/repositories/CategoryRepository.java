72fe17de9e650f2a12afc3724aceea2fd49c988b
Daniel Galvan <danieldg.1989@gmail.com>
2025-10-13 21:33:00 -0300

    Category

---
+++ src/main/java/com/daniel/demo/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.daniel.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.daniel.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
