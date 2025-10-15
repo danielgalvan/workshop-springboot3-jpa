061dc4852f576a253c2f3fad9d9de3538d2a94b8
Daniel Galvan <danieldg.1989@gmail.com>
2025-10-14 22:45:23 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/daniel/demo/entities/Order.java
+++ src/main/java/com/daniel/demo/entities/Order.java
@@ -2,7 +2,9 @@ package com.daniel.demo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.daniel.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +36,9 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 
@@ -68,11 +74,6 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
 	
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
@@ -83,7 +84,16 @@ public class Order implements Serializable {
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
---
+++ src/main/java/com/daniel/demo/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.daniel.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.daniel.demo.entities.OrderItem;
+import com.daniel.demo.entities.pk.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+}
