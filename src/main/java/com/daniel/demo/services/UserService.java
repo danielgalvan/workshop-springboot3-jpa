10f0d5ed086ca8d3445e90aeea5bad12f54ffffe
Daniel Galvan <danieldg.1989@gmail.com>
2025-10-16 20:22:51 -0300

    Exception handling - update

--- src/main/java/com/daniel/demo/services/UserService.java
+++ src/main/java/com/daniel/demo/services/UserService.java
@@ -13,6 +13,8 @@ import com.daniel.demo.repositories.UserRepository;
 import com.daniel.demo.services.exceptions.DatabaseException;
 import com.daniel.demo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, user);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
